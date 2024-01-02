03872062afee9a04a31adbd33549df73614d3d4d
shahirulprojects <shahiruliimalo@gmail.com>
2024-01-02 17:33:36 +0800

    docs:add some notes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@
 //dia guna bracket[] which use the concept of array destructuring
 //bracket tu accept dua benda which is name of that state and a setter function
 //orang selalu namakan setter function tu with "set" + nama state tu sebab dia adalah setter function untuk state tu
-//contoh: 
+//contoh:
 //nama State:counter
 //name setter function:setCounter
 //it is VERY important to note that we should NEVER modify/mutate the state
@@ -42,9 +42,9 @@
 //CODE2 SEBELUM NI
 
 // import './App.css';
-// const Person=(props)=>{ 
-//   return(               
-//     <> 
+// const Person=(props)=>{
+//   return(
+//     <>
 //       <h1>Name:{props.name}</h1>
 //       <h2>Last Name:{props.lastName}</h2>
 //       <h2>Age:{props.age}</h2>
@@ -58,35 +58,36 @@
 //     <div className="App">
 //       <Person name={"Kim"} lastName={"Dahyun"} age={25}/>
 //       <Person name={"Minatozaki"} lastName={"Sana"} age={26}/>
-      
+
 //     </div>
 //   );
 // }
 // export default App;
 
-import {useState,useEffect} from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-const App=()=> {
-  const [counter,setCounter]=useState(0); //0 tu initial value
+const App = () => {
+  const [counter, setCounter] = useState(0); //0 tu initial value
 
-  useEffect(()=>{
-    setCounter(100) //the counter is initially set to 0 dalam useState but because of the setCounter(100) in the useEffect function,the counter is now set to 100
-  },[]) //dependency array ni untuk kita set bila kita nak useEffect ni run.
-        //kalau kita biar kosong,dia akan just run time initial load je (maksudnya sekali je dia run)
-        //kalau kita letak something like [counter],dia akan run everytime variable counter tu change
-
+  useEffect(() => {
+    setCounter(100); //the counter is initially set to 0 dalam useState but because of the setCounter(100) in the useEffect function,the counter is now set to 100
+  }, []); //dependency array ni untuk kita set bila kita nak useEffect ni run.
+  //kalau kita biar kosong,dia akan just run time initial load je (maksudnya sekali je dia run)
+  //kalau kita letak something like [counter],dia akan run everytime variable counter tu change
 
   //prevCount tu kita guna untuk jadikan button minus and button plus tu tolak or tambah 1 each time button tu kena click.saja bubuh nama prevCount,boleh je bubuh nama apa2
   return (
     <div className="App">
-      <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
       <h1>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
-      
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
-}
+};
 
 export default App;
-
